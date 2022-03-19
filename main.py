1fdc65e3d8da2c6e6902e250810a5416cc71fcfe
Lincoln Jeong <lincoln.jeong@navercorp.com>
2022-03-19 17:18:40 +0900

    test4

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from fastapi import FastAPI
 from os import environ
+import json
+import requests
 
 app = FastAPI()
 
@@ -11,8 +13,31 @@ def read_root():
     return {"Hello": "World", "master": environ['MASTER_NAME']}
 
 
-@app.post("/", status_code=200)
-def post_root():
+@app.post("/vibe", status_code=200)
+def post_root(payload):
+    payload = json.loads(payload)
+    events = payload['events']
+    for event in events:
+        if event['type'] is not 'message':
+            continue
+        reply_token = event['replyToken']
+        user_id = event['source']['userId']
+        message = event['message']['text']
+
+        messages = {
+            "type": "text",
+            "text": "user_id: {}\noriginal_message: {}".format(user_id, message)
+        }
+
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {}'.format(environ['CHANNEL_ACCESS_TOKEN'])
+        }
+        body = {
+            'replyToken': reply_token,
+            'messages': messages
+        }
+        requests.post(url='https://api.line.me/v2/bot/message/reply', headers=headers, data=body)
     return {"Hello": "World"}
 
 
