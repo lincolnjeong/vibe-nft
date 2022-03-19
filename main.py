9523315514b184596afbae7e27fd15cb3344df02
Lincoln Jeong <lincoln.jeong@navercorp.com>
2022-03-19 18:09:33 +0900

    g

--- main.py
+++ main.py
@@ -1,22 +1,70 @@
-from typing import Optional
-
-from fastapi import FastAPI
-from os import environ
-import json
-import requests
 import logging
+
+import os
+from os import environ
+from typing import List, Optional
+import requests
+
+from fastapi import FastAPI, APIRouter, HTTPException, Header, Request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import TextMessage, MessageEvent, TextSendMessage, StickerMessage, \
+    StickerSendMessage
+from pydantic import BaseModel
+
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+
 
 app = FastAPI()
 logger = logging.getLogger("app")
 
 
+class Line(BaseModel):
+    destination: str
+    events: List[Optional[None]]
+
+
+@router.post("/line")
+async def callback(request: Request, x_line_signature: str = Header(None)):
+    body = await request.body()
+    try:
+        handler.handle(body.decode("utf-8"), x_line_signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="chatbot handle body error.")
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def message_text(event):
+    print("!!!!!!!!!!!!!!!!!!!!!!")
+    print(event)
+    print("!!!!!!!!!!!!!!!!!!!!!!")
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
+
+
+@handler.add(MessageEvent, message=StickerMessage)
+def sticker_text(event):
+    # Judge condition
+    line_bot_api.reply_message(
+        event.reply_token,
+        StickerSendMessage(package_id='6136', sticker_id='10551379')
+    )
+
+
+
+    
 @app.get("/")
 def read_root():
     return {"Hello": "World", "master": environ['MASTER_NAME']}
 
 
 @app.post("/vibe/", status_code=200)
-def post_root(payload):
+def post_root(request: Request, x_line_signature: str = Header(None)):
+    payload = request.body()
     logger.debug(payload)
     events = payload['events']
     for event in events:
