e60058cb7b614f1715d338f4b8cb7f79c59219db
Lincoln Jeong <lincoln.jeong@navercorp.com>
2022-03-20 19:24:50 +0900

    d

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -33,6 +33,185 @@ async def callback(request: Request, x_line_signature: str = Header(None)):
     return 'OK'
 
 
+template = {
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://musicmeta-phinf.pstatic.net/album/005/141/5141786.jpg?type=r480Fll&v=202203201451",
+            "size": "5xl",
+            "aspectMode": "cover",
+            "aspectRatio": "150:196",
+            "gravity": "center",
+            "flex": 1
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "image",
+                "url": "https://musicmeta-phinf.pstatic.net/album/000/194/194629.jpg?type=r480Fll&v=202203201451",
+                "size": "full",
+                "aspectMode": "cover",
+                "aspectRatio": "150:98",
+                "gravity": "center"
+              },
+              {
+                "type": "image",
+                "url": "https://musicmeta-phinf.pstatic.net/album/005/242/5242739.jpg?type=r480Fll&v=202203201451",
+                "size": "full",
+                "aspectMode": "cover",
+                "aspectRatio": "150:98",
+                "gravity": "center"
+              }
+            ],
+            "flex": 1
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "horizontal",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "contents": [
+                  {
+                    "type": "span",
+                    "text": "Tracks",
+                    "weight": "bold",
+                    "color": "#000000"
+                  },
+                  {
+                    "type": "span",
+                    "text": "    | "
+                  },
+                  {
+                    "type": "span",
+                    "text": "봄비 - 라일락, 봄봄 - 아이유, 허슬허슬허슬허슬허슬 - 도끼"
+                  }
+                ],
+                "size": "sm",
+                "wrap": true
+              },
+              {
+                "type": "text",
+                "contents": [
+                  {
+                    "type": "span",
+                    "text": "Tracks",
+                    "weight": "bold",
+                    "color": "#000000"
+                  },
+                  {
+                    "type": "span",
+                    "text": "    | "
+                  },
+                  {
+                    "type": "span",
+                    "text": "봄비 - 라일락, 봄봄 - 아이유, 허슬허슬허슬허슬허슬 - 도끼"
+                  }
+                ],
+                "size": "sm",
+                "wrap": true
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Maker | U2oeoASefqwefQWEFqwef",
+                    "size": "sm",
+                    "color": "#bcbcbc",
+                    "contents": [
+                      {
+                        "type": "span",
+                        "text": "Maker ID"
+                      },
+                      {
+                        "type": "span",
+                        "text": " | "
+                      },
+                      {
+                        "type": "span",
+                        "text": "U1iriowef92"
+                      }
+                    ]
+                  }
+                ],
+                "spacing": "sm",
+                "margin": "md"
+              },
+              {
+                "type": "box",
+                "layout": "baseline",
+                "contents": [
+                  {
+                    "type": "text",
+                    "text": "Maker | U2oeoASefqwefQWEFqwef",
+                    "size": "sm",
+                    "color": "#bcbcbc",
+                    "contents": [
+                      {
+                        "type": "span",
+                        "text": "Tx"
+                      },
+                      {
+                        "type": "span",
+                        "text": " | "
+                      },
+                      {
+                        "type": "span",
+                        "text": "Txdddd"
+                      }
+                    ]
+                  }
+                ],
+                "margin": "none",
+                "spacing": "none"
+              }
+            ]
+          }
+        ],
+        "spacing": "xl",
+        "paddingAll": "20px"
+      }
+    ],
+    "paddingAll": "0px"
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "토큰 확인",
+          "uri": "http://linecorp.com/"
+        }
+      }
+    ]
+  }
+}
+
+flex_message = FlexSendMessage(
+  template
+)
+
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
     print("!!!!!!!!!!!!!!!!!!!!!!")
@@ -41,8 +220,10 @@ def message_text(event):
     message = f"msg: {event.message.text}\nuser_id: {event.source.user_id}"
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=message)
+        # TextSendMessage(text=message)
+        flex_message
     )
+
 
 
 @handler.add(MessageEvent, message=StickerMessage)
--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import json
 import random
 import string
 import time
-
 import requests
 
 from signature_generator import SignatureGenerator
@@ -20,13 +19,24 @@ def read_json(file_path):
     return data
 
 
-class Utils:
+def get_track_meta(query):
+    url = f'https://apis.naver.com/vibeWeb/musicapiweb/v3/search/track.json?query={query}&start=1&display=100&sort=RELEVANCE'
+    meta = requests.get(url).json()['response']['result']['tracks'][0]
+    track_id = meta['trackId']
+    track_title = meta['trackTitle']
+    artist_name = ", ".join([i['artistName'] for i in meta['artists']])
+    album_title = meta['album']['albumTitle']
+    album_image = meta['album']['imageUrl']
+    return {'track_id': track_id, 'track_title': track_title, 'artist_name': artist_name, 'album_title': album_title, 'album_image': album_image}
+
+
+class BlackChainUtils:
     def __init__(self, config):
         sg = SignatureGenerator()
         self.get_signature = sg.generate
         self.config = config
 
-    def get_api(self, path):
+    def get_headers(self, path, method, body=None):
         nonce = ''.join(
             random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(8))
         timestamp = int(round(time.time() * 1000))
@@ -34,7 +44,16 @@ class Utils:
             'service-api-key': self.config['service_api_key'],
             'nonce': nonce,
             'timestamp': str(timestamp),
-            'signature': self.get_signature(method='GET', path=path, nonce=nonce, timestamp=timestamp,
-                                            secret=self.config['service_api_secret']),
+            'signature': self.get_signature(method=method, path=path, nonce=nonce, timestamp=timestamp,
+                                            secret=self.config['service_api_secret'], body=body) if body is not None else self.get_signature(method=method, path=path, nonce=nonce, timestamp=timestamp,
+                                            secret=self.config['service_api_secret'])
         }
+        return headers
+
+    def get_api(self, path):
+        headers = self.get_headers(path, method='GET')
         return requests.get(self.config['endpoint'] + path, headers=headers)
+
+    def post_api(self, path, request_body):
+        headers = self.get_headers(path, method='POST', body=request_body)
+        return requests.post(self.config['endpoint'] + path, headers=headers, json=request_body)
