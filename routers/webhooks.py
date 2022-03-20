68382677eb80f7929b2ace567f59232ace6b43cc
Lincoln Jeong <lincoln.jeong@navercorp.com>
2022-03-21 03:01:12 +0900

    d

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -64,7 +64,7 @@ def message_text(event):
         )
     elif status == 200:
         msg = make_msg(msg, user_info)
-        nft_result = mint_nft(user_info['user_id'], str(msg), str(msg))
+        nft_result = mint_nft(user_info['user_id'], user_info['display_name'][:20], str(msg))
         print(nft_result, nft_result.json())
         if (nft_result.status_code >= 200) & (nft_result.status_code <= 300):
             msg['tx_id'] = nft_result.json()['responseData']['txHash']
