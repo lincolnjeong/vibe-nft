b9331bac380e4419105a6e355b82673338797ab2
Lincoln Jeong <lincoln.jeong@navercorp.com>
2022-03-21 01:21:11 +0900

    d

--- message/templates.py
+++ message/templates.py
@@ -76,156 +76,8 @@ def make_image(images):
         }]
 
 
-def get_template():
-    return json.loads("""{
-        "type": "bubble",
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "box",
-                    "layout": "horizontal",
-                    "contents": {
-                        "type": "image",
-                        "url": "image_url",
-                        "size": "5xl",
-                        "aspectMode": "cover",
-                        "aspectRatio": "300:196",
-                        "gravity": "center",
-                        "flex": 1
-                    }
-                },
-                {
-                    "type": "box",
-                    "layout": "horizontal",
-                    "contents": [
-                        {
-                            "type": "box",
-                            "layout": "vertical",
-                            "contents": [
-                                {
-                                    "type": "text",
-                                    "contents": [
-                                        {
-                                            "type": "span",
-                                            "text": "Tracks",
-                                            "weight": "bold",
-                                            "color": "#000000"
-                                        },
-                                        {
-                                            "type": "span",
-                                            "text": " | "
-                                        },
-                                        {
-                                            "type": "span",
-                                            "text": "track_info"
-                                        }
-                                    ],
-                                    "size": "sm",
-                                    "wrap": True
-                                },
-                                {
-                                    "type": "text",
-                                    "contents": [
-                                        {
-                                            "type": "span",
-                                            "text": "위 정보로 NFT가 발행되었습니다.",
-                                            "weight": "bold",
-                                            "color": "#000000"
-                                        }
-                                    ],
-                                    "size": "sm",
-                                    "wrap": True
-                                },
-                                {
-                                    "type": "box",
-                                    "layout": "baseline",
-                                    "contents": [
-                                        {
-                                            "type": "text",
-                                            "text": "",
-                                            "size": "sm",
-                                            "color": "#bcbcbc",
-                                            "contents": [
-                                                {
-                                                    "type": "span",
-                                                    "text": "Maker ID"
-                                                },
-                                                {
-                                                    "type": "span",
-                                                    "text": " | "
-                                                },
-                                                {
-                                                    "type": "span",
-                                                    "text": "user_info"
-                                                }
-                                            ]
-                                        }
-                                    ],
-                                    "spacing": "sm",
-                                    "margin": "md"
-                                },
-                                {
-                                    "type": "box",
-                                    "layout": "baseline",
-                                    "contents": [
-                                        {
-                                            "type": "text",
-                                            "text": "",
-                                            "size": "sm",
-                                            "color": "#bcbcbc",
-                                            "contents": [
-                                                {
-                                                    "type": "span",
-                                                    "text": "Tx ID"
-                                                },
-                                                {
-                                                    "type": "span",
-                                                    "text": " | "
-                                                },
-                                                {
-                                                    "type": "span",
-                                                    "text": "tx_id"
-                                                }
-                                            ]
-                                        }
-                                    ],
-                                    "margin": "none",
-                                    "spacing": "none"
-                                }
-                            ]
-                        }
-                    ],
-                    "spacing": "xl",
-                    "paddingAll": "20px"
-                }
-            ],
-            "paddingAll": "0px"
-        },
-        "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "button",
-                    "action": {
-                        "type": "uri",
-                        "label": "토큰 확인",
-                        "uri": "https://explorer.blockchain.line.me/cashew/transaction/"
-                    }
-                }
-            ]
-        }
-    }""")
-
-
-def message_contents(msg):
-    print('msg----------')
-    print(msg)
-    print('-------------')
-    images = msg['image']
-    template = {
+template = """
+{
   "type": "bubble",
   "body": {
     "type": "box",
@@ -234,7 +86,40 @@ def message_contents(msg):
       {
         "type": "box",
         "layout": "horizontal",
-        "contents": make_image(images)
+        "contents": [
+          {
+            "type": "image",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip7.jpg",
+            "size": "5xl",
+            "aspectMode": "cover",
+            "aspectRatio": "150:196",
+            "gravity": "center",
+            "flex": 1
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "image",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip8.jpg",
+                "size": "full",
+                "aspectMode": "cover",
+                "aspectRatio": "150:98",
+                "gravity": "center"
+              },
+              {
+                "type": "image",
+                "url": "https://scdn.line-apps.com/n/channel_devcenter/img/flexsnapshot/clip/clip9.jpg",
+                "size": "full",
+                "aspectMode": "cover",
+                "aspectRatio": "150:98",
+                "gravity": "center"
+              }
+            ],
+            "flex": 1
+          }
+        ]
       },
       {
         "type": "box",
@@ -245,54 +130,98 @@ def message_contents(msg):
             "layout": "vertical",
             "contents": [
               {
-                "type": "image",
-                "url": images[0],
-                "aspectMode": "cover",
-                "size": "full"
-              }
-            ],
-            "cornerRadius": "100px",
-            "width": "72px",
-            "height": "72px"
-          },
-          {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
                 "type": "text",
                 "contents": [
                   {
                     "type": "span",
-                    "text": "brown_05",
+                    "text": "Tracks",
                     "weight": "bold",
                     "color": "#000000"
                   },
                   {
                     "type": "span",
-                    "text": "     "
+                    "text": " | "
                   },
                   {
                     "type": "span",
-                    "text": "I went to the Brown&Cony cafe in Tokyo and took a picture"
+                    "text": "도끼 블랙넛 어쩌구 저쩌구"
                   }
                 ],
                 "size": "sm",
-                "wrap": True
+                "wrap": true
               },
               {
                 "type": "box",
-                "layout": "baseline",
+                "layout": "vertical",
                 "contents": [
                   {
                     "type": "text",
-                    "text": "1,140,753 Like",
+                    "contents": [
+                      {
+                        "type": "span",
+                        "text": "위 정보로 NFT가 발행되었습니다."
+                      }
+                    ],
                     "size": "sm",
-                    "color": "#bcbcbc"
+                    "wrap": true
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "size": "sm",
+                        "color": "#bcbcbc",
+                        "contents": [
+                          {
+                            "type": "span",
+                            "text": "Maker",
+                            "weight": "bold"
+                          },
+                          {
+                            "type": "span",
+                            "text": " | "
+                          },
+                          {
+                            "type": "span",
+                            "text": "maker_info"
+                          }
+                        ]
+                      }
+                    ],
+                    "spacing": "sm",
+                    "margin": "md"
+                  },
+                  {
+                    "type": "box",
+                    "layout": "baseline",
+                    "contents": [
+                      {
+                        "type": "text",
+                        "size": "sm",
+                        "color": "#bcbcbc",
+                        "contents": [
+                          {
+                            "type": "span",
+                            "text": "Tx Hash",
+                            "weight": "bold"
+                          },
+                          {
+                            "type": "span",
+                            "text": " | "
+                          },
+                          {
+                            "type": "span",
+                            "text": "tx_info"
+                          }
+                        ]
+                      }
+                    ],
+                    "spacing": "none",
+                    "margin": "none"
                   }
-                ],
-                "spacing": "sm",
-                "margin": "md"
+                ]
               }
             ]
           }
@@ -302,8 +231,43 @@ def message_contents(msg):
       }
     ],
     "paddingAll": "0px"
+  },
+  "footer": {
+    "type": "box",
+    "layout": "horizontal",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "토큰 확인",
+          "uri": "https://explorer.blockchain.line.me/cashew/transaction/"
+        }
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "uri",
+          "label": "음악 듣기",
+          "uri": "https://vibe.naver.com/"
+        }
+      }
+    ]
   }
 }
+"""
+
+
+def get_template():
+    return json.dumps(json.loads(template))
+
+
+def message_contents(msg):
+    print('msg----------')
+    print(msg)
+    print('-------------')
+    images = msg['image']
+    template = get_template()
     print('---------------')
     print(template)
     print('---------------')
